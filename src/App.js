05cdddc143fbcc91413faa470e5ba46da3ac9aeb
Br0k3nh4nd012 <gokulakrishnan312@gmail.com>
2021-03-21 17:17:35 +0530

    second

--- src/App.js
+++ src/App.js
@@ -66,15 +66,15 @@ class App extends React.Component {
   }
   render() { 
     return ( 
-      <div className="App">
-      <div class="bg-yellow-400 h-auto">
-        <h1 class="bg-yellow-400 text-center text-3xl m-auto mt-7 mb-7 rounded-xl max-w-2xl h-auto font-mono p-6 ">TODO LIST</h1>
+      <div className="App" >
+      <div class="bg-gradient-to-tr from-yellow-200 to-yellow-400 py-5 min-h-screen mb-56">
+        <h1 class="bg-yellow-400 text-center text-3xl m-auto mt-5 mb-7 rounded-xl max-w-2xl h-auto font-mono py-6">TODO LIST</h1>
 
   <div class="grid justify-items-center">
     <form onSubmit={this.addItem}>
     <div class="justify-between space-x-2 bg">
         <input class="mb-12 rounded-md h-11 px-4 w-96" type="text" value= {this.state.currentItem.text} onChange={this.handleInput} placeholder="Enter task"/>
-        <button class="bg-yellow-50 h-11 px-5 rounded-md " type="submit">Add</button>
+        <button class="bg-yellow-200 h-11 px-5 rounded-md hover:bg-yellow-600" type="submit">Add</button>
     </div>
     </form>
     <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
--- src/ListItems.js
+++ src/ListItems.js
@@ -6,12 +6,64 @@ class ListItems extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            subtask: 0,
+            subtask: false,
             subtaskkey: Date.now(),
+            nesteditem:{
+                text:"",
+                key:"",
+                parentkey:"",
+            },
+            nestitems:[],
          }
+         this.additem = this.additem.bind(this);
+        this.handleinputnest = this.handleinputnest.bind(this);
+    }
+    handleinputnest(text,parentkey){
+        this.setState({
+            nesteditem:{
+              text: text,
+              key: Date.now(),
+              parentkey: parentkey, 
+            }
+          })
+    }
+    additem(e){
+        e.preventDefault();
+        console.log(this.state.nesteditem);
+    const newnestitem = this.state.nesteditem;
+    if(newnestitem.text !==""){
+      const items = [...this.state.nestitems, newnestitem];
+    this.setState({
+      nestitems: items,
+      nesteditem:{
+        text:'',
+        key:'',
+        parentkey:''
+      }
+    })
+    }
+    }
+    
+    deleteItem(key){
+        const filteredItems= this.state.nestitems.filter(item =>
+            item.key!==key);
+          this.setState({
+            nestitems: filteredItems
+          })
     }
 
-    
+    setUpdate(text,key){
+        const items = this.state.nestitems;
+        items.map(item=>{      
+        if(item.key===key){
+            console.log(item.key +"    "+key)
+            item.text= text;
+        }
+        })
+        this.setState({
+        nestitems: items
+        })
+    }
     // componentDidUpdate(){
     //     this.setState({subtask:false});
     // }
@@ -47,15 +99,39 @@ class ListItems extends React.Component {
                         ()=>{
                             k=this.state.subtask+1;
                             this.setState({
-                                subtask:k,
+                                subtask:true,
                                 subtaskkey:item.key
                             });
                         }
                     }
                 >+</button>
                 {console.log(this.state.subtask)}
-              {(this.state.subtask>=1 && this.state.subtaskkey===item.key)?<NestedList k={this.state.subtask}/> : null}
+              {(this.state.subtask && this.state.subtaskkey===item.key)?<div class="bg-white rounded-md w-auto flex justify-between px-5 py-2 mb-3 ">
+        <input class="outline-none" onChange={(e)=>{this.handleinputnest(e.target.value,item.key)}} value={this.state.nesteditem.text} placeholder="enter subtask"/>
+        <button class="bg-yellow-200 px-3 py-1 rounded-xl hover:bg-yellow-500" type="submit" onClick={this.additem}>done</button>
+        <button class=" hover:bg-red-500 rounded-3xl px-2 ml-2 " type="submit" onClick={()=>{this.setState({subtask:false})}
+}>x</button>
+      </div> : null}
+              {console.log(this.state.nestitems)}
               
+
+              {this.state.nestitems.filter(nestitem => nestitem.parentkey===item.key).map(eachsub => (
+                  <div class="block mt-2">
+                      <div>
+                    <input class="outline-none space-x-4" value={eachsub.text} onChange={
+                        (e)=>{
+                            this.setUpdate(e.target.value,eachsub.key)
+                        }
+                    } placeholder="enter subtask"/>
+                    <button class="bg-yellow-400 px-4 py-1 rounded-xl hover:bg-yellow-500 " onClick={
+                        ()=>{
+                            this.deleteItem(eachsub.key)
+                        }
+                    }>Delete</button>
+                    </div>
+                    </div>
+              ))}
+
             </div>
            
           </div>
